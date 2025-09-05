691e4925f086e0b159c64d1d6deece1dc2cad9ba
Aditya Munjale <munjaleaditya13@gmail.com>
2025-09-05 10:45:42 +0530

    Creating route like production level

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 
+const userRoute = require("./routes/user.routes");
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use("/user", userRoute);
 
 app.listen("3000", () => {
   console.log("Server is runnig on port 3000");
